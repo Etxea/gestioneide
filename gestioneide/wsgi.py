7ca67eea85dfebfbea244d2042eaddf08a459d1d
root <jonlatorremartinez@gmail.com>
2016-03-05 20:30:58 +0100

    Portada para IES Laredo

--- gestioneide/wsgi.py
+++ gestioneide/wsgi.py
@@ -7,10 +7,13 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gestioneide.settings")
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+
+
 
 application = get_wsgi_application()
