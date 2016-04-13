57e524d830bb432890427129e37ea2c4d5eab766
Jon Latorre <info@etxea.net>
2016-04-13 22:35:08 +0200

    Jugand con las form y formset de notas

--- evaluacion/forms.py
+++ evaluacion/forms.py
@@ -5,32 +5,30 @@ from gestioneide.models import *
 
 from django.forms.models import modelformset_factory
 
-
-NotaFormSet = modelformset_factory(Nota,exclude=('trimestre','asistencia','id'),extra=0)
-ElementayNotaFormSet = modelformset_factory(Nota,exclude=('trimestre','asistencia','id'),extra=0)
-UpperNotaFormSet = modelformset_factory(Nota,exclude=('trimestre','asistencia','id'),extra=0)
-FaltaFormSet = modelformset_factory(Falta,exclude=('mes','asistencia','id'),extra=0)
-
-class BaseNotaFormSet(BaseModelFormSet):
-    def __init__(self, *args, **kwargs):
-        super(BaseNotaFormSet, self).__init__(*args, **kwargs)
-        self.queryset = Nota.objects.filter(trimestre==kwargs['trimestre'])
+#~ class BaseNotaFormSet(BaseModelFormSet):
+    #~ def __init__(self, *args, **kwargs):
+        #~ super(BaseNotaFormSet, self).__init__(*args, **kwargs)
+        #~ self.queryset = Nota.objects.filter(trimestre==kwargs['trimestre'])
 
 class NotaCreateForm(ModelForm):
     class Meta:
         model = Nota
-        #fields = ( "username", "email", "telefono1", "telefono2", "first_name", "last_name" )
-        fields = '__all__'
+        fields = ['control','control_np']
         
-class UpperNotaCreateFrom(NotaCreateForm):
+class UpperNotaCreateFrom(ModelForm):
     class Meta:
         model = Nota
-        fields = ["grammar", "grammar_np"]
+        fields = ['reading','reading_np','useofenglish','useofenglish_np','writing','writing_np','speaking','speaking_np','listenning','listenning_np']
         
 
-class ElementaryNotaCreateForm(NotaCreateForm):
+class ElementaryNotaCreateForm(ModelForm):
     class Meta:
         model = Nota
-        fields = ["writing", "writing_np"]
+        fields = ['reading','reading_np', 'grammar','grammar_np', 'writing','writing_np',  'speaking','speaking_np', 'listenning','listenning_np']
         
 
+NotaFormSet = modelformset_factory(Nota,form=NotaCreateForm,extra=0)
+ElementayNotaFormSet = modelformset_factory(Nota,form=ElementaryNotaCreateForm,extra=0)
+UpperNotaFormSet = modelformset_factory(Nota,form=UpperNotaCreateFrom,extra=0)
+
+FaltaFormSet = modelformset_factory(Falta,exclude=('mes','asistencia','id'),extra=0)
