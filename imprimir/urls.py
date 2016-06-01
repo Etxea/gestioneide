77438cf379f88aed6b0db794e1b3895e88b6e5fe
Jon Latorre <jonlatorremartinez@gmail.com>
2016-06-01 06:56:20 +0200

    Cambiado el sistema de impresion

--- imprimir/urls.py
+++ imprimir/urls.py
@@ -3,11 +3,11 @@ from django.contrib.auth.decorators import login_required, permission_required
 from views import *
 
 urlpatterns = [
-    url(r'grupos$',login_required(ImprimirGrupos), name="imprimir_grupos"),
+    url(r'grupos$',login_required(ImprimirGrupos.as_view()), name="imprimir_grupos"),
     url(r'grupos/alumnos/$',login_required(ImprimirGruposAlumnos.as_view()), name="imprimir_grupos_alumnos"),
     url(r'grupos/alumnos/id/(?P<grupo_id>\d+)/$',login_required(ImprimirGruposAlumnos.as_view()),  name="imprimir_grupo_unico_alumnos"),
-    url(r'grupos/planilla/asistencia/(?P<mes>\d+)/$',login_required(ImprimirGruposPlanillaAsistencia), name="imprimir_grupos_planilla_asistencia"),
+    url(r'grupos/planilla/asistencia/(?P<mes>\d+)/$',login_required(ImprimirGruposPlanillaAsistencia.as_view()), name="imprimir_grupos_planilla_asistencia"),
     url(r'alumno/matricula/(?P<alumno_id>\d+)/$',login_required(ImprimirAlumnoMatricula.as_view()), name="imprimir_alumno_matricula"),
-    url(r'alumno/octavilla/(?P<alumno_id>\d+)/$',login_required(ImprimirAlumnoOctavilla), name="imprimir_alumno_octavilla"),
+    url(r'alumno/octavilla/(?P<alumno_id>\d+)/$',login_required(ImprimirAlumnoOctavilla.as_view()), name="imprimir_alumno_octavilla"),
     url(r'asistencia/(?P<asistencia_id>\d+)/horario$',login_required(ImprimirAsistenciaHorario.as_view()), name="imprimir_asistencia_horario"),
 ]
--- imprimir/views.py
+++ imprimir/views.py
@@ -4,84 +4,20 @@ import os
 import calendar
 
 from django.conf import settings
-from django.http import HttpResponse
-from django.template import Context
-from django.template.loader import get_template
 
-from xhtml2pdf import pisa
 from wkhtmltopdf.views import PDFTemplateView
 
 from gestioneide.models import *
 
-def link_callback(uri, rel):
-    """
-    Convert HTML URIs to absolute system paths so xhtml2pdf can access those
-    resources
-    """
-    print "Convirtiendo...",uri,rel
-    # use short variable names
-    sUrl = settings.STATIC_URL      # Typically /static/
-    sRoot = settings.STATIC_ROOT    # Typically /home/userX/project_static/
-    mUrl = settings.MEDIA_URL       # Typically /static/media/
-    mRoot = settings.MEDIA_ROOT     # Typically /home/userX/project_static/media/
-
-    # convert URIs to absolute system paths
-    if uri.startswith(mUrl):
-        path = os.path.join(mRoot, uri.replace(mUrl, ""))
-    elif uri.startswith(sUrl):
-        path = os.path.join(sRoot, uri.replace(sUrl, ""))
-    else:
-        return uri  # handle absolute uri (ie: http://some.tld/foo.png)
-
-    # make sure that file exists
-    if not os.path.isfile(path):
-            print "Tenemos el path que falla",path
-            return path
-    return path
-
-def ImprimirGrupos(request):
-    data = {}
-    year = Year.objects.get(activo=True)
-    grupos = Grupo.objects.filter(year=year)
-    data['grupo_list'] = grupos
-    data['year'] = year.__unicode__()
-    template = get_template('grupos_pdf.html')
-    html = template.render(Context(data))
-    f = open(os.path.join(settings.MEDIA_ROOT, 'test.pdf'), "w+b")
-    pisaStatus = pisa.CreatePDF(html, dest=f, link_callback=link_callback)
-    f.seek(0)
-    pdf = f.read()
-    f.close()
-    return HttpResponse(pdf, content_type='application/pdf')
-
-#~ def ImprimirGruposAlumnos(request):
-    #~ data = {}
-    #~ year = Year.objects.get(activo=True)
-    #~ if request.method == 'POST':
-        #~ if 'listagrupos' in request.POST:
-            #~ lista=request.POST.getlist('listagrupos')
-            #~ print "tenemos la lista de grupos"
-            #~ print lista
-            #~ grupos = Grupo.objects.filter(year=year).filter(id__in=lista)
-        #~ if 'grupo_id' in request.POST:
-            #~ grupo_id = request.POST['grupo_id']
-            #~ print "Buscamos el grupo ",grupo_id
-            #~ grupos = Grupo.objects.filter(year=year).filter(id=grupo_id)
-    #~ else:
-        #~ grupos = Grupo.objects.filter(year=year).annotate(Count('asistencia')).filter(asistencia__count__gt=0)
-#~ 
-    #~ data['grupo_list'] = grupos
-    #~ data['year'] = year.__unicode__()
-    #~ template = get_template('grupos_alumnos_pdf.html')
-    #~ html = template.render(Context(data))
-    #~ f = open(os.path.join(settings.MEDIA_ROOT, 'grupos_alumnos_%s.pdf'%year), "w+b")
-    #~ pisaStatus = pisa.CreatePDF(html, dest=f, link_callback=link_callback)
-    #~ print "Tebenemos pisastatus",pisaStatus
-    #~ f.seek(0)
-    #~ pdf = f.read()
-    #~ f.close()
-    #~ return HttpResponse(pdf, content_type='application/pdf')
-
+class ImprimirGrupos(PDFTemplateView):
+    filename='grupos.pdf'
+    template_name = "grupos_pdf.html"    
+    def get_context_data(self, **kwargs):
+        context = super(ImprimirGrupos, self).get_context_data(**kwargs)
+        year = Year.objects.get(activo=True)
+        context['year'] = year.__unicode__()
+        context['grupo_list'] = Grupo.objects.filter(year=year).annotate(Count('asistencia')).filter(asistencia__count__gt=0)
+        return context
 
 class ImprimirGruposAlumnos(PDFTemplateView):
     filename='my_pdf.pdf'
@@ -122,40 +58,32 @@ class ImprimirAlumnoMatricula(PDFTemplateView):
         context['alumno'] = alumno
         return context
     
-def ImprimirAlumnoOctavilla(request,alumno_id):
-    data = {}
-    alumno = Alumno.objects.get(id=alumno_id)
-    data['alumno'] = alumno
-    template = get_template('octavilla_alumno_pdf.html')
-    html = template.render(Context(data))
-    f = open(os.path.join(settings.MEDIA_ROOT, 'octavilla_alumno_%s.pdf'%alumno_id), "w+b")
-    pisaStatus = pisa.CreatePDF(html, dest=f, link_callback=link_callback)
-    f.seek(0)
-    pdf = f.read()
-    f.close()
-    return HttpResponse(pdf, content_type='application/pdf')    
-    
+class ImprimirAlumnoOctavilla(PDFTemplateView):
+    filename='octavilla_alumno_pdf.pdf'
+    template_name = 'octavilla_alumno_pdf.html'
+    cmd_options = {"page-size": "A5","orientation": "landscape"}
+    def get_context_data(self, **kwargs):
+        context = super(ImprimirAlumnoOctavilla, self).get_context_data(**kwargs)
+        alumno_id= kwargs['alumno_id']
+        alumno = Alumno.objects.get(id=alumno_id)
+        context['alumno'] = alumno
+        return context
 
-def ImprimirGruposPlanillaAsistencia(request,mes):
-    year = Year.objects.get(activo=True)
-    mes=int(mes)
-    data = {}
-    grupos = Grupo.objects.filter(year=year)
-    data['grupo_list'] = grupos
-    #FIXME esto habria que sacarlo de algun lado
-    ano = year.start_year
-    if mes < 8 :
-        ano = ano + 1 
-    data['ano'] = ano
-    mes_nombre = calendar.month_name[mes]
-    data['mes'] = mes
-    data['mes_nombre'] = calendar.month_name[mes]
-
-    template = get_template('grupos_planilla_asistencia_pdf.html')
-    html = template.render(Context(data))
-    f = open(os.path.join(settings.MEDIA_ROOT, 'test.pdf'), "w+b")
-    pisaStatus = pisa.CreatePDF(html, dest=f, link_callback=link_callback)
-    f.seek(0)
-    pdf = f.read()
-    f.close()
-    return HttpResponse(pdf, content_type='application/pdf')
+class ImprimirGruposPlanillaAsistencia(PDFTemplateView):
+    filename='grupos_planilla_asistencia.pdf'
+    template_name = 'grupos_planilla_asistencia_pdf.html'
+    cmd_options = {"orientation": "landscape"}
+    def get_context_data(self, **kwargs):
+        context = super(ImprimirGruposPlanillaAsistencia, self).get_context_data(**kwargs)
+        mes= int(kwargs['mes'])
+        year = Year.objects.get(activo=True)
+        context['year'] = year.__unicode__()
+        context['grupo_list'] = Grupo.objects.filter(year=year).annotate(Count('asistencia')).filter(asistencia__count__gt=0)
+        ano = year.start_year
+        if mes < 8 :
+            ano = ano + 1 
+        context['ano'] = ano
+        mes_nombre = calendar.month_name[mes]
+        context['mes'] = mes
+        context['mes_nombre'] = calendar.month_name[mes]
+        return context
