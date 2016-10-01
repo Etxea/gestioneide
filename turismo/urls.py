f3efb0e188a03b70b4ee923389ed7c75d5850d03
Jon Latorre <jonlatorremartinez@gmail.com>
2016-10-01 16:56:50 +0200

    Gestion clases turismo

--- turismo/urls.py
+++ turismo/urls.py
@@ -9,10 +9,13 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r"^$", TurismoView.as_view(), name="turismo_home"),
+    url(r"clase/nueva/asignatura/(?P<asignatura_id>\d+)/$", TurismoClaseAsignaturaCreateView.as_view(), name="turismo_clase_nueva_asignatura"),
+    url(r"clase/nueva$", TurismoClaseCreateView.as_view(), name="turismo_clase_nueva"),
+    url(r"clase/(?P<pk>\d+)/editar$", TurismoClaseUpdateView.as_view(), name="turismo_clase_editar"),
+    url(r"clase/(?P<pk>\d+)/borrar$", TurismoClaseDeleteView.as_view(), name="turismo_clase_borrar"),
     url(r"asignatura/nueva/$", TurismoAsignaturaCreateView.as_view(), name="turismo_asignatura_nueva"),
     url(r"asignatura/(?P<pk>\d+)/$", TurismoAsignaturaDetailView.as_view(), name="turismo_asignatura_detalle"),
     url(r"asistencia/nueva/$", TurismoAsistenciaCreateView.as_view(), name="turismo_asistencia_nueva"),
     url(r"asistencia/(?P<pk>\d+)/$", TurismoAsistenciaDetailView.as_view(), name="turismo_asistencia_detalle"),
-    url(r"clase/nueva$", TurismoClaseCreateView.as_view(), name="turismo_clase_nueva"),
-    url(r"clase/(?P<pk>\d+)/borrar$", TurismoClaseDeleteView.as_view(), name="turismo_clase_borrar"),
+    
 ]
--- turismo/views.py
+++ turismo/views.py
@@ -26,6 +26,7 @@ class TurismoAsignaturaCreateView(CreateView):
 @method_decorator(permission_required('gestioneide.turismo_view',raise_exception=True),name='dispatch')    
 class TurismoAsignaturaDetailView(DetailView):
     model = TurismoAsignatura
+    context_object_name = 'asignatura'
     template_name = "turismo/asignatura_detalle.html"
 
 @method_decorator(permission_required('gestioneide.turismo_view',raise_exception=True),name='dispatch')
@@ -34,7 +35,7 @@ class TurismoAsistenciaCreateView(CreateView):
     fields = "__all__"
     template_name = "turismo/asistencia_nueva.html"
     success_url = "/turismo"
-
+    
 @method_decorator(permission_required('gestioneide.turismo_view',raise_exception=True),name='dispatch')    
 class TurismoAsistenciaDetailView(DetailView):
     model = TurismoAsistencia
@@ -45,11 +46,34 @@ class TurismoClaseCreateView(CreateView):
     model = TurismoClase
     fields = "__all__"
     template_name = "turismo/clase_nueva.html"
-    success_url = "/turismo"
+    def get_success_url(self):
+        return reverse_lazy('turismo_asignatura_detalle', args = (self.object.asignatura.id,))
+
+@method_decorator(permission_required('gestioneide.turismo_view',raise_exception=True),name='dispatch')
+class TurismoClaseAsignaturaCreateView(TurismoClaseCreateView):
+    template_name = "turismo/clase_nueva_form.html"
+    def get_success_url(self):
+        return reverse_lazy('turismo_asignatura_detalle', args = (self.object.asignatura.id,))
+    def get_initial(self):
+        super(TurismoClaseAsignaturaCreateView, self).get_initial()
+        asignatura = TurismoAsignatura.objects.get(pk=self.kwargs['asignatura_id'])
+        user = self.request.user
+        self.initial = {"asignatura":asignatura.id}
+        return self.initial
+
+@method_decorator(permission_required('gestioneide.turismo_view',raise_exception=True),name='dispatch')
+class TurismoClaseUpdateView(UpdateView):
+    model = TurismoClase
+    fields = "__all__"
+    template_name = "turismo/clase_nueva_form.html"
+    def get_success_url(self):
+        return reverse_lazy('turismo_asignatura_detalle', args = (self.object.asignatura.id))
 
 @method_decorator(permission_required('gestioneide.turismo_view',raise_exception=True),name='dispatch')
 class TurismoClaseDeleteView(DeleteView):
     model = TurismoClase
     fields = "__all__"
     template_name = "turismo/clase_borrar.html"
-    success_url = "/turismo"
+    def get_success_url(self):
+        return reverse_lazy('turismo_asignatura_detalle', args = (self.object.asignatura.id,))
+
