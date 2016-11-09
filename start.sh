c1da9c217febf82b0944aed51a974db5a2278311
Jon Latorre <jonlatorremartinez@gmail.com>
2016-11-09 18:15:07 +0100

    Pruebas con el entrypoint de docker

--- start.sh
+++ start.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 # Start Gunicorn processes
+python manage.py migrate                  # Apply database migrations
+python manage.py collectstatic --noinput  # Collect static files
+
 echo Starting Gunicorn.
 exec gunicorn gestioneide.wsgi:application \
     --bind 0.0.0.0:8000 \
