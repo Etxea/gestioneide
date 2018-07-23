e718d987a06a46b9c7ff15477e2b9179ab31df45
Jon Latorre <info@etxea.net>
2018-07-23 17:29:40 +0200

    Actualizar passwd todos los profes

---
+++ gestioneide/management/commands/resetalluserpassword.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+__author__ = 'patataman'
+from gestioneide.models import *
+from django.core.management.base import BaseCommand, CommandError
+
+
+class Command(BaseCommand):
+    help = 'Actualiza la contraseña de todos los profesores y la envía al email asociado.'
+
+    def add_arguments(self, parser):
+        parser.add_argument('profesor_id', type=int)
+
+    def handle(self, *args, **options):
+        for profesor in Profesor.objects.all():
+                self.stdout.write(self.style.SUCCESS('Trabajando con el profesor %s' % (profesor)))
+                if profesor.user.is_active:
+                    profesor.update_user_password()
+                else:
+                    self.stdout.write(
+                        self.style.SUCCESS('Profesor dado de BAJA %s' % (profesor)))
+
+
+
