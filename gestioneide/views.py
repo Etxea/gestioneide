c0ae168f3cd01336c8a05f723d78c60a9f1ab97c
Jon Latorre <jlatorre@irontec.com>
2021-06-12 08:20:31 +0200

    admin dash

--- gestioneide/views.py
+++ gestioneide/views.py
@@ -1,14 +1,19 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from gestioneide.models import Alumno
+from matriculas.models import MatriculaCurso, MatriculaEide, MatriculaLinguaskill
 
-from django.shortcuts import render, redirect
-from django.views.generic import ListView, DetailView, FormView, TemplateView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from django.contrib.auth.models import User
-from django.core.urlresolvers import reverse, reverse_lazy
-from django.http import HttpResponseRedirect
-from django.utils.decorators import method_decorator
-from django.contrib.auth.decorators import login_required, permission_required
-
-class HomeView(TemplateView):
+class HomeView(TemplateView,LoginRequiredMixin):
+    login_url = '/accounts/login/'
+    redirect_field_name = 'redirect_to'
     template_name='home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(TemplateView,self).get_context_data(**kwargs)
+        context['ultimos_alumnos'] = Alumno.objects.all().order_by('-id')[:5]
+        context['ultimas_matriculas_cursos'] = MatriculaCurso.objects.all().order_by('-id')[:5]
+        context['ultimas_matriculas_eide'] = MatriculaEide.objects.all().order_by('-id')[:5]
+        context['ultimas_matriculas_ls'] = MatriculaLinguaskill.objects.all().order_by('-id')[:5]
+        return context
