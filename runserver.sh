324c57681da001e2d4df5e7aa765973584fa9f6a
Jon Latorre <jlatorre@irontec.com>
2021-10-04 17:49:29 +0200

    Newrelic on runserver

--- runserver.sh
+++ runserver.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 source ./bin/activate
-./manage.py runserver 9000
-NEW_RELIC_CONFIG_FILE=newrelic.ini newrelic-admin run-program ./manage.py runserver 0.0.0.0:9000
+if test -f newrelic.ini
+then
+	NEW_RELIC_CONFIG_FILE=newrelic.ini newrelic-admin run-program ./manage.py runserver 0.0.0.0:9000
+else
+
+	./manage.py runserver 9000
+fi
